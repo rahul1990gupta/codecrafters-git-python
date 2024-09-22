b2971d2caac9193a4790e07db8e01ccfff6975fc
Rahul Gupta <rahul1990gupta@gmail.com>
2024-09-22 11:42:35 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,10 +1,9 @@
 import sys
 import os
-
+import zlib
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
 
     # Uncomment this block to pass the first stage
     #
@@ -16,6 +15,19 @@ def main():
         with open(".git/HEAD", "w") as f:
             f.write("ref: refs/heads/main\n")
         print("Initialized git directory")
+    elif command == "cat-file":
+        sha = sys.argv[3]
+        path = f".git/objects/{sha[:2]}/{sha[2:]}"
+        
+        """
+        An object starts with a header that specifies its type: blob, commit, tag or tree. This header is followed by an ASCII space (0x20), then the size of the object in bytes as an ASCII number, then null (0x00) (the null byte), then the contents of the objec
+        """
+        
+        with open(path, 'rb') as f:
+            raw = zlib.decompress(f.read())
+        fc_index = raw.find(b"\x00")
+        print(raw[fc_index+1:].decode(), end="")
+
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
