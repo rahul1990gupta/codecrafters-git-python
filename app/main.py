8ebefa78d98511da4e420a3ce63b4de31d9bae0c
Rahul Gupta <rahul1990gupta@gmail.com>
2024-09-22 15:34:07 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import zlib
+import hashlib
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -21,36 +22,79 @@ def main():
         print(content.decode(), end="")
     elif command == "hash-object":
         fname = sys.argv[3]
-        with open(fname, 'rb') as f:
-            content = f.read()
-        size = len(content)
-        header = f"blob {size}"
-        obj_content = header.encode() + b"\x00" + content 
-        
-        import hashlib
-        sha = hashlib.sha1(obj_content).hexdigest()
-        print(sha)
-        os.mkdir(f".git/objects/{sha[:2]}")
-        
-        with open(f".git/objects/{sha[:2]}/{sha[2:]}", "wb") as f:
-            f.write(zlib.compress(obj_content))
+        sha = write_object(fname)
+        print(sha.hexdigest())
+
     elif command == "ls-tree":
         sha = sys.argv[3]
         content = read_object(sha)
         
-
         buff = content
-        
         while b"\x00" in buff:
             nb_index = buff.find(b"\x00")
             oname = buff[:nb_index].split(b"\x20")[1]
             print(oname.decode())
             
-            offset = nb_index + 20 
             buff = buff[nb_index + 20:]
-            
+    elif command == "write-tree":
+        sha = write_directory(os.getcwd())
+        print(sha.hexdigest())
     else:
         raise RuntimeError(f"Unknown command #{command}")
+
+
+def write_directory(dir_name):
+    
+    buff = b""
+    entries = list(os.scandir(dir_name))
+    entries.sort(key= lambda x: x.name)
+    for entry in entries:
+        if entry.name == ".git":
+            continue
+
+        if entry.is_file():
+            mode = "100644"
+        elif entry.is_dir():
+            mode = "40000"
+        else: 
+            ValueError("mode not supported")
+        
+        name = entry.name
+        if entry.is_file():
+            sha  = write_object(f"{dir_name}/{entry.name}")
+        elif entry.is_dir():
+            sha = write_directory(f"{dir_name}/{entry.name}")
+        
+        sha_20 = int.to_bytes(int(sha.hexdigest(), base=16), length=20, byteorder="big")
+        
+        buff += f"{mode} {name}".encode() + b"\x00" + sha_20
+        len(buff)
+
+    sha = serialize("tree", buff)
+    return sha
+
+
+def serialize(ctype, content):
+    size = len(content)
+    header = f"{ctype} {size}"
+    obj_content = header.encode() + b"\x00" + content 
+    
+    sha_raw = hashlib.sha1(obj_content)
+    sha = sha_raw.hexdigest()
+    
+    os.makedirs(f".git/objects/{sha[:2]}", exist_ok=True)
+    
+    with open(f".git/objects/{sha[:2]}/{sha[2:]}", "wb") as f:
+        f.write(zlib.compress(obj_content))
+    
+    return sha_raw
+
+
+def write_object(fname):
+    with open(fname, 'rb') as f:
+        content = f.read()
+    sha = serialize("blob", content)
+    return sha
 
 
 def read_object(sha):
